81d960acf5544321a60f15331e3a75d35dacbc68
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-07-19 16:36:16 +0300

    Написал контроллеры, формы, шаблоны и url пути для модели Customers

--- mailing_service/urls.py
+++ mailing_service/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
+from django.views.generic import TemplateView
+from mailing_service import views as custom_views
+
+
+app_name = 'mailing_service'
 
 urlpatterns = [
-    path(...),
+    path('', TemplateView.as_view(template_name='index.html'), name='homepage'),
+    path('mailing-menu/', TemplateView.as_view(template_name='mailing_service/mailing_menu.html'), name='mailing_menu'),
+    path('mailing-menu/users', custom_views.CustomersListView.as_view(), name='customers_list'),
+    path('mailing-menu/users/create', custom_views.CustomerCreateView.as_view(), name='customer_create'),
+    path('mailing-menu/users/update/<int:pk>', custom_views.CustomerUpdateView.as_view(), name='customer_update'),
+    path('mailing-menu/users/delete/<int:pk>', custom_views.CustomerDeleteView.as_view(), name='customer_delete'),
 ]
---
+++ mailing_service/views/customer_views.py
@@ -0,0 +1,28 @@
+from django.urls import reverse_lazy
+from django.views import generic
+from mailing_service.forms import CustomerForm
+from mailing_service.models import Customers
+
+
+class CustomersListView(generic.ListView):
+    model = Customers
+    template_name = 'mailing_service/customers_table.html'
+
+
+class CustomerCreateView(generic.CreateView):
+    form_class = CustomerForm
+    template_name = 'mailing_service/customer_creating.html'
+    success_url = reverse_lazy('mailing_service:customers_list')
+
+
+class CustomerUpdateView(generic.UpdateView):
+    model = Customers
+    form_class = CustomerForm
+    template_name = 'mailing_service/customer_creating.html'
+    success_url = reverse_lazy('mailing_service:customers_list')
+
+
+class CustomerDeleteView(generic.DeleteView):
+    model = Customers
+    template_name = 'mailing_service/confirm_delete.html'
+    success_url = reverse_lazy('mailing_service:customers_list')
