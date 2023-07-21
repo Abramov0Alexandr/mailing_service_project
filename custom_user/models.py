cf671d44a9f81c34c7c6ef88d84031d09e0ca91f
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-07-21 12:24:51 +0300

    Создал модель CustomUser и зарегистрировал ее в админке

---
+++ custom_user/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class CustomUser(AbstractUser):
+
+    USER_STATUS = [
+        (True, 'Активирован'),
+        (False, 'Не активирован')
+    ]
+
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Email')
+    fullname = models.CharField(max_length=100, verbose_name='ФИО пользователя')
+    avatar = models.ImageField(upload_to=f'users/{email}', verbose_name='Автар', **NULLABLE)
+
+    is_active = models.BooleanField(default=False, verbose_name='Статус активации')
+    verification_key = models.CharField(default='Не верифицирован', verbose_name='Ключ активации')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
