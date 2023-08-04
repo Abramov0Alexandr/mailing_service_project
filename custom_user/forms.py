f05b37cdf9ba19a61a9630134de23b81d697a42c
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-08-05 01:12:13 +0300

    Рефакторинг, пересоздал БД

--- custom_user/forms.py
+++ custom_user/forms.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
 from custom_user.models import CustomUser
 
 
@@ -12,6 +14,12 @@ class StyleFormMixin:
 
 class UserRegistrationForm(StyleFormMixin, UserCreationForm):
 
+    email = forms.EmailField(widget=forms.TextInput(attrs={'type': 'email'}))
+
     class Meta:
         model = CustomUser
         fields = ('email', 'fullname', 'avatar')
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        return email.lower()
